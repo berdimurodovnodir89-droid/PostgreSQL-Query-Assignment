5bff238cb9ea4a473db49fd67e31695d19024b71
Nodirbek <berdimurodovnodir89@gmail.com>
2026-02-26 12:00:51 +0500

    grouping

--- filter.sql
+++ filter.sql
@@ -1,3 +1,15 @@
 SELECT * FROM library 
 where 
-    genre = 'Self-help'
+    genre = 'Self-help';
+
+
+
+SELECT * FROM library
+where 
+published_year > 2000;
+
+
+
+SELECT * FROM library
+WHERE
+price > 20.00;
---
+++ sorting.sql
@@ -0,0 +1,16 @@
+SELECT max(price) FROM library;
+
+
+
+SELECT * FROM library
+ORDER BY published_year LIMIT 3;
+
+
+SELECT  author
+FROM library
+GROUP BY author
+ORDER BY author ASC ;
+
+
+
+
